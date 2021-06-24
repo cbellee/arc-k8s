addd6831b3810e1aee40a6ab89c570b3acf204c8
Chris Bellee <{ID}+{username}@users.noreply.github.com>
2021-06-24 13:37:00 +1000

    removed stuff

--- bicep/deploy.sh
+++ bicep/deploy.sh
@@ -1,8 +1,8 @@
-LOCATION=australiaeast
-ALIAS=cbellee
+LOCATION=<your location>
+ALIAS=<your alias>
 RG_NAME=k8s-arc-workshop-${ALIAS}-rg
 ADMIN_USER_NAME=localadmin
-ADMIN_PASSWORD=P@ssword1234567890
+ADMIN_PASSWORD=<your password>
 SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
 
 az group create --location $LOCATION --resource-group $RG_NAME
